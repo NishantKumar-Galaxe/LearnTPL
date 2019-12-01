9c647f2f2770e5e2410e8a489e3eda4d0e623c3e
Nishant Kumar <nkumar1@galaxe.com>
2019-12-01 15:31:31 +0530

    Add new code

---
+++ Example1/Example2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Example1
+{
+    public static class Example2
+    {
+
+        // Thread t1
+        public static void print(bool isEven)
+        {
+            if (isEven)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine($"Even Number {i}");
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Console.WriteLine($"Odd Number {i}");
+                    }
+                }
+            }
+        }
+    }
+}
--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading;
 
 namespace Example1
 {
@@ -7,35 +8,54 @@ namespace Example1
     {
         static void Main(string[] args)
         {
+
+
+            // Example-1 starts here 
+
             //Process.Start("notepad.exe", "d:\\Hello.txt");
 
-            var app = new Process
-            {
-                StartInfo = {
-                    FileName=@"notepad.exe",
-                    Arguments="d:\\Hello.txt"
-                }
-            };
+            //var app = new Process
+            //{
+            //    StartInfo = {
+            //        FileName=@"notepad.exe",
+            //        Arguments="d:\\Hello.txt"
+            //    }
+            //};
 
-            app.Start();
-            app.PriorityClass = ProcessPriorityClass.High;
+            //app.Start();
+            //app.PriorityClass = ProcessPriorityClass.High;
 
-            Console.WriteLine("Going to kill process");
+            //Console.WriteLine("Going to kill process");
 
-            app.WaitForInputIdle(5000);
-         
-
-            var allrunningProcesses = Process.GetProcesses();
-            foreach (var process in allrunningProcesses)
-            {
-                if (process.ProcessName.Equals("notepad"))
-                {
-                    process.Kill();
-                }
-            }
+            //app.WaitForInputIdle(5000);
 
 
-            Console.WriteLine("No more notepad instances at all....");
+            //var allrunningProcesses = Process.GetProcesses();
+            //foreach (var process in allrunningProcesses)
+            //{
+            //    if (process.ProcessName.Equals("notepad"))
+            //    {
+            //        process.Kill();
+            //    }
+            //}
+
+
+            //Console.WriteLine("No more notepad instances at all....");
+            //Console.ReadLine();
+
+            // Example-1 Ends here 
+
+            // Example-2 starts here 
+
+
+            Thread T1 = new Thread(() => Example2.print(true));
+            T1.Start();
+
+            Example2.print(false);
+
+            // Example-2 Ends here 
+
+
             Console.ReadLine();
         }
     }
