a7dbac4f385e0cb74601a138c271b3ec1c2ff44e
Nishant Kumar <nkumar1@galaxe.com>
2019-12-01 19:36:01 +0530

    code

--- Example1/Example2.cs
+++ Example1/Example2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Example1
 {
@@ -11,6 +12,8 @@ namespace Example1
         // Thread t1
         public static void print(bool isEven)
         {
+            Console.WriteLine($"Current Thread ID is:: {Thread.CurrentThread.ManagedThreadId}");
+
             if (isEven)
             {
                 for (int i = 0; i < 100; i++)
@@ -31,6 +34,25 @@ namespace Example1
                     }
                 }
             }
+        }
+
+        public static void RunForEver()
+        {
+            while (true) { Thread.Sleep(1000); }
+        }
+
+
+        public static int PrintTask(CancellationToken token)
+        {
+            Console.WriteLine($"Is Thread Pool Thread: {Thread.CurrentThread.IsThreadPoolThread}");
+            int total = default(int);
+            for (int i = 1; i <= 50; i++)
+            {
+                total = total + i;
+                Console.WriteLine($"Current task id={Task.CurrentId}, value is {i}, total is {total}");
+            }
+
+            return total;
         }
     }
 }
--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Example1
 {
@@ -48,15 +49,63 @@ namespace Example1
             // Example-2 starts here 
 
 
-            Thread T1 = new Thread(() => Example2.print(true));
-            T1.Start();
+            //Thread T1 = new Thread(() => Example2.print(true));
+            //T1.Start();
 
-            Example2.print(false);
+            //Example2.print(false);
 
             // Example-2 Ends here 
 
+            //Thread T2 = new Thread(() => Example2.RunForEver());
+            //T2.IsBackground = true;
+            //T2.Start();
 
-            Console.ReadLine();
+            // Example-3 starts here 
+
+            //Task
+            //Task.Run(() => Example2.PrintTask());
+            //Task.Run(() => Example2.PrintTask());
+
+            var cts = new CancellationTokenSource();
+
+
+            var res = Task.Factory.StartNew(() => Example2.PrintTask(cts.Token), cts.Token);
+            var res1 = Task.Factory.StartNew(() => Example2.PrintTask(cts.Token), cts.Token);
+
+
+            Console.WriteLine($"Result of first task is {res.Result}");
+            Console.WriteLine($"Result of second task is {res1.Result}");
+          
+            // Example-3 Ends here 
+
+
+
+            // Example-4 starts here 
+
+            // Example-4 Ends here 
+
+
+            // Example-5 starts here 
+
+            // Example-5 Ends here 
+
+
+            // Example-6 starts here 
+
+            // Example-6 Ends here 
+
+
+            // Example-7 starts here 
+
+            // Example-7 Ends here 
+
+
+            // Example-8 starts here 
+
+            // Example-8 Ends here 
+
+
+            Console.Read();
         }
     }
 }
