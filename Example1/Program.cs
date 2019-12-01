4cd8a5d413685d9133b92a9f2ec775bc8fd63ad0
Nishant Kumar <nkumar1@galaxe.com>
2019-12-01 13:51:12 +0530

    Add project files.

---
+++ Example1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Process.Start("notepad.exe", "d:\\Hello.txt");
+
+            var app = new Process
+            {
+                StartInfo = {
+                    FileName=@"notepad.exe",
+                    Arguments="d:\\Hello.txt"
+                }
+            };
+
+            app.Start();
+            app.PriorityClass = ProcessPriorityClass.High;
+
+            Console.WriteLine("Going to kill process");
+
+            app.WaitForInputIdle(5000);
+         
+
+            var allrunningProcesses = Process.GetProcesses();
+            foreach (var process in allrunningProcesses)
+            {
+                if (process.ProcessName.Equals("notepad"))
+                {
+                    process.Kill();
+                }
+            }
+
+
+            Console.WriteLine("No more notepad instances at all....");
+            Console.ReadLine();
+        }
+    }
+}
